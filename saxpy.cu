bf363d37623153f50510eb1ad99c2bcc841d1a71
George Hotz <geohot@gmail.com>
2022-05-21 15:05:32 -0700

    progress

--- saxpy.cu
+++ saxpy.cu
@@ -2,19 +2,27 @@
 #include <cuda.h>
 #include <signal.h>
 #include <unistd.h>
+#include <assert.h>
 #include <sys/mman.h>
 
+//#define BROKEN
+
 extern "C" {
-extern const unsigned long long fatbinData[346];
+extern const unsigned long long fatbinData[351];
 }
 
 __global__
-void saxpy(int n, float a, float *x, float *y)
+void saxpy(int n, float a, float *x, float *y, int bob)
 {
   int i = blockIdx.x*blockDim.x + threadIdx.x;
   if (i < n) y[i] = a*x[i] + y[i];
 }
 
+void dump_gpu_ctrl() {
+  printf("***** read\n");
+  uint32_t *ptr = (uint32_t*)0x200400000;
+  while (ptr != (uint32_t*)0x203600000) { if (*ptr != 0) printf("%p: %16lx\n", ptr, *ptr); ++ptr; }
+}
 
 int main(int argc, char *argv[]) {
   int N = 1<<20;
@@ -37,9 +45,11 @@ int main(int argc, char *argv[]) {
   CUmodule mod = 0;
   cuModuleLoadFatBinary(&mod, fatbinData);
 
+  // CUfunc_st*
   CUfunction saxpy_f = 0;
-  cuModuleGetFunction(&saxpy_f, mod, "_Z5saxpyifPfS_");
-  printf("function 0x%X\n", saxpy_f);
+  cuModuleGetFunction(&saxpy_f, mod, "_Z5saxpyifPfS_i");
+  printf("function %p\n", saxpy_f);
+  assert(saxpy_f != 0);
 
   printf("***** print memory\n");
   size_t free_byte, total_byte;
@@ -61,35 +71,59 @@ int main(int argc, char *argv[]) {
   cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
 
   // Perform SAXPY on 1M elements
-  printf("***** launch\n");
+  printf("***** unmap\n");
   //raise(SIGTRAP);
   //saxpy<<<(N+255)/256, 256>>>(N, 2.0f, d_x, d_y);
 
+  int bob = 0x1337f00d;
   float ratio = 2.0f;
-  void *args[] = { &N, &ratio, &d_x, &d_y };
+  void *args[] = { &N, &ratio, &d_x, &d_y, &bob};
 
   munmap((void*)0x200200000, 0x200400000-0x200200000);    // /dev/nvidiactl
-  //munmap((void*)0x200400000, 0x200600000-0x200400000);    // /dev/nvidia0    NEEDED
-  //munmap((void*)0x200600000, 0x203600000-0x200600000);    // /dev/nvidiactl  NEEDED
-  //munmap((void*)0x204600000, 0x204800000-0x204600000);    // /dev/nvidiactl  NEEDED
+
+#ifdef BROKEN
+  // these are needed for launch
+  // * Nvidia device node(nvidia#) maps device's BAR memory,
+  // * Nvidia control node(nvidiactrl) maps system memory.
+  munmap((void*)0x200400000, 0x200600000-0x200400000);    // /dev/nvidia0    NEEDED
+  munmap((void*)0x200600000, 0x203600000-0x200600000);    // /dev/nvidiactl  NEEDED
+  //munmap((void*)0x204600000, 0x204800000-0x204600000);    // /dev/nvidiactl  NEEDED (READ ONLY)
+  mprotect((void*)0x204600000, 0x204800000-0x204600000, PROT_READ);
+
+  void *ret = mmap((void*)0x200400000, 0x203600000-0x200400000, PROT_READ | PROT_WRITE, MAP_FIXED | MAP_SHARED | MAP_ANON, -1, 0);
+  assert(ret == (void*)0x200400000);
+#endif
+  memset((void*)0x200400000, 0, 0x203600000-0x200400000);
+
   munmap((void*)0x204800000, 0x204a00000-0x204800000);    // /dev/nvidiactl
   munmap((void*)0x204a00000, 0x204c00000-0x204a00000);    // /dev/nvidia-uvm
   munmap((void*)0x204c00000, 0x204e00000-0x204c00000);    // /dev/nvidiactl
   //munmap((void*)0x205000000, 0x205200000-0x205000000);    // /dev/nvidiactl  NEEDED AFTER LAUNCH
+  mprotect((void*)0x205000000, 0x205200000-0x205000000, PROT_READ);
 
-  char buf[0x10000];
+  /*char buf[0x10000];
   FILE *f = fopen("/proc/self/maps", "rb");
   //FILE *f = fopen("/proc/self/pagemap", "rb");
   buf[fread(buf, 1, sizeof(buf), f)] = '\0';
-  printf("%s\n", buf);
+  printf("%s\n", buf);*/
 
   //while (1) sleep(1);
 
   // calls into /lib/x86_64-linux-gnu/libcuda.so.515.43.04
+  printf("***** launch\n");
   cuLaunchKernel(saxpy_f, (N+255)/256, 1, 1, 256, 1, 1, 0, 0, args, NULL);
+  cuStreamSynchronize(0);
+  dump_gpu_ctrl();
 
-  printf("***** exit memcpy\n");
+  //printf("***** sync\n");
+  //memset((void*)0x200400000, 0, 0x203600000-0x200400000);
+  //cuStreamSynchronize(0);
+  //dump_gpu_ctrl();
+
+  printf("***** exit memcpy %p -> %p\n", d_y, y);
+  memset((void*)0x200400000, 0, 0x203600000-0x200400000);
   cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
+  dump_gpu_ctrl();
 
   float maxError = 0.0f;
   for (int i = 0; i < N; i++)
